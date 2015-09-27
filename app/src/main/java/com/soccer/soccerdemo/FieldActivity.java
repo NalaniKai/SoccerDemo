93377936b71c53b25f2cf1a7b06f13178dfa6b74
NalaniKai <megan.nalani@gmail.com>
2015-09-27 12:48:50 -0700

    Views can change back and forth between field activity and main

--- app/src/main/java/com/soccer/soccerdemo/FieldActivity.java
+++ app/src/main/java/com/soccer/soccerdemo/FieldActivity.java
@@ -4,14 +4,30 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class FieldActivity extends ActionBarActivity {
+
+    private Button stats;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_field);
+
+        stats = (Button) findViewById(R.id.return_stats);
+
+        stats.setOnClickListener(new returnToStatsListener());
+    }
+
+    public class returnToStatsListener implements View.OnClickListener {
+
+        @Override
+        public void onClick(View v) {
+            finish();
+        }
     }
 
     @Override
