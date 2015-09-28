cc1fc57d0abee714a637368c53192493ccd62c05
NalaniKai <megan.nalani@gmail.com>
2015-09-28 12:06:24 -0700

    Drop downs for players in selected teams in PlayersActivity works

--- app/src/main/java/com/soccer/soccerdemo/PlayersActivity.java
+++ app/src/main/java/com/soccer/soccerdemo/PlayersActivity.java
@@ -27,16 +27,24 @@ public class PlayersActivity extends ActionBarActivity {
     private ImageButton right;
     private ImageButton left;
 
-    //member lists
-    private EditText memb1;
-    private EditText memb2;
-
     //spinner drop down for teams
-    private Spinner spinner1;
-    private Spinner spinner2;
+    private Spinner spinner1Team;
+    private Spinner spinner2Team;
 
-    ArrayAdapter<String> adapter; //adapter for team spinners
+    //member spinners
+    private Spinner spinnerMemb1;
+    private Spinner spinnerMemb2;
+
+    ArrayAdapter<String> adapterTeam; //adapter for team spinners
     ArrayList<String> team_list; //holds team names
+
+    //adapters for players
+    ArrayAdapter<String> adapterPlayers1;
+    ArrayAdapter<String> adapterPlayers2;
+
+    //ArrayLists for players
+    ArrayList<String> player1List;
+    ArrayList<String> player2List;
 
     HashMap<String, Player> players; //players hashmap
 
@@ -44,6 +52,21 @@ public class PlayersActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_players);
+
+        //initialize team spinners
+        spinner1Team = (Spinner) findViewById(R.id.spinner_team1);
+        spinner2Team = (Spinner) findViewById(R.id.spinner_team2);
+
+        //members spinners
+        spinnerMemb1 = (Spinner) findViewById(R.id.spinner_members1);
+        spinnerMemb2 = (Spinner) findViewById(R.id.spinner_members2);
+
+        //initialize button to go back to MainActivity
+        statsBtn = (Button) findViewById(R.id.statsButton);
+
+        //buttons to exchange players between teams
+        right = (ImageButton) findViewById(R.id.arrow_right);
+        left = (ImageButton) findViewById(R.id.arrow_left);
 
         team_list = new ArrayList<>(); //initialize array list for teams
 
@@ -54,46 +77,32 @@ public class PlayersActivity extends ActionBarActivity {
         team_list = intent.getStringArrayListExtra("hi"); //get array list of team names
         players = (HashMap<String, Player>) intent.getSerializableExtra("players"); //get player hashmap
 
-        //initialize team spinners
-        spinner1 = (Spinner) findViewById(R.id.spinner_team1);
-        spinner2 = (Spinner) findViewById(R.id.spinner_team2);
-
-        //initialize button to go back to MainActivity
-        statsBtn = (Button) findViewById(R.id.statsButton);
-
-        //buttons to exchange players between teams
-        right = (ImageButton) findViewById(R.id.arrow_right);
-        left = (ImageButton) findViewById(R.id.arrow_left);
-
-        //members list
-        memb1 = (EditText) findViewById(R.id.membList1);
-        memb2 = (EditText) findViewById(R.id.membList2);
-
         //initialize adapter with list of team names and connect to spinners
-        adapter = new ArrayAdapter<>(this,
+        adapterTeam = new ArrayAdapter<>(this,
                 android.R.layout.simple_list_item_1,
                 android.R.id.text1,
                 team_list);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner1.setAdapter(adapter);
-        spinner2.setAdapter(adapter);
+        adapterTeam.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner1Team.setAdapter(adapterTeam);
+        spinner2Team.setAdapter(adapterTeam);
 
         //set listeners
         statsBtn.setOnClickListener(new statsListener());
-        spinner1.setOnItemSelectedListener(new s1Listener());
-        spinner2.setOnItemSelectedListener(new s2Listener());
+        spinner1Team.setOnItemSelectedListener(new s1TeamListener());
+        spinner2Team.setOnItemSelectedListener(new s2TeamListener());
     }
 
     /*
      * class: s2Listener            Listens for selected item in spinner on the right side of the
      *                              screen and displays the players in that team.
      */
-    private class s2Listener implements AdapterView.OnItemSelectedListener {
+    private class s2TeamListener implements AdapterView.OnItemSelectedListener {
 
         @Override
         public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
             String selectedTeam = parent.getItemAtPosition(position).toString(); //get selected team
 
+            displayPlayers2(selectedTeam); //display players in team 2
             String playerNames = ""; //string to hold player names
 
             Set<String> keys = players.keySet(); //get all player keys
@@ -104,8 +113,6 @@ public class PlayersActivity extends ActionBarActivity {
                     playerNames += k + "\n"; //stores players in selected team
                 }
             }
-
-            memb2.setText(playerNames); //displays players in selected team
         }
 
         @Override
@@ -115,33 +122,68 @@ public class PlayersActivity extends ActionBarActivity {
     }
 
     /*
+     * method: displayPlayers2          Displays players in team selected on right drop down.
+     */
+    public void displayPlayers2 (String team) {
+        player2List = new ArrayList<>(); //new arrayList for players
+
+        Set<String> keys = players.keySet(); //get keys for all players
+
+        for(String k: keys) {
+            if(players.get(k).getTeamName().equals(team)) {
+                player2List.add(k);
+            }
+        }
+
+        adapterPlayers2 = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1,
+                android.R.id.text1,
+                player2List);
+        adapterPlayers2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerMemb2.setAdapter(adapterPlayers2);
+    }
+
+    /*
      * class: s1Listener            Listens for selected item in spinner on the left side of the
      *                              screen and displays the players in that team.
      */
-    private class s1Listener implements AdapterView.OnItemSelectedListener {
+    private class s1TeamListener implements AdapterView.OnItemSelectedListener {
 
         @Override
         public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
             String selectedTeam = parent.getItemAtPosition(position).toString(); //get selected team
 
-            String playerNames = ""; //string to hold player names
-
-            Set<String> keys = players.keySet(); //gets all player keys
-
-            //check for players in team
-            for(String k: keys) {
-                if(players.get(k).getTeamName().equals(selectedTeam)) {
-                    playerNames += k + "\n"; //stores players in selected team
-                }
-            }
-
-            memb1.setText(playerNames); //displays players in selected team
+            displayPlayers1(selectedTeam); //display players in team
         }
 
         @Override
         public void onNothingSelected(AdapterView<?> parent) {
 
         }
+    }
+
+    /*
+     * method: displayPlayers           Displays players in left drop down for members.
+     */
+    public void displayPlayers1(String team) {
+        player1List = new ArrayList<>(); //new player list
+
+        Set<String> keys = players.keySet(); //gets all player keys
+
+        //check for players in team
+        for(String k: keys) {
+            if(players.get(k).getTeamName().equals(team)) {
+                player1List.add(k); //gets players in team
+            }
+        }
+
+        //set adapter and spinner for players
+        adapterPlayers1 = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1,
+                android.R.id.text1,
+                player1List);
+        adapterPlayers1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerMemb1.setAdapter(adapterPlayers1);
     }
 
     /*
